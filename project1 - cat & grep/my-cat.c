e0b848ded3b65cce34901341d277d3c995006a5a
Njoroge Waweru <njorogealexw@gmail.com>
2018-09-18 22:13:02 +0000

    my-cat added

---
+++ project1 - cat & grep/my-cat.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(int argc, char** argv )
+{
+    // If no file is passed exit with error status 0
+    if (argc < 2)
+    {
+        exit(0);
+    }
+
+    // Read the files and print them out on the console in order
+    for (int index = 1; index < argc; index++)
+    {
+        FILE* file_pointer = fopen(argv[index], "r");
+        if(!file_pointer) 
+        {
+            perror("cannot open file\n");
+            return EXIT_FAILURE;
+        }
+
+        int c;
+        while((c = getc(file_pointer)) !=EOF)
+        {
+            putchar(c);
+        } 
+        printf("\n\n");
+
+        fclose(file_pointer);       
+    }
+
+    exit(0);
+}
